593056e6dfe7b7fd09228005e77d57574146811c
Majid Valizadeh <majid.valiz7adeh@gmail.com>
2025-06-10 13:28:05 +0330

    Add sorting by request count for top 10 paths

--- Bash-Scripting/nginx-log-analyser.sh
+++ Bash-Scripting/nginx-log-analyser.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Show top 10 IP addresses with most requests
 DEFAULT_LOG="./nginx-access.log"
 LOG_FILE="${1:-$DEFAULT_LOG}"
 
@@ -12,6 +11,13 @@ fi
 
 echo "Top 10 IP addresses with the most requests:"
 results=( $(awk '{print $1}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 10) )
+for ((i=0; i<${#results[@]}; i+=2)); do
+    echo "$((i/2+1)). ${results[i+1]} - ${results[i]} requests"
+done
+
+echo
+echo "Top 10 paths with the most requests:"
+results=( $(awk '{print $7}' "$LOG_FILE" | sort | uniq -c | sort -nr | head -n 10 | awk '{count=$1; $1=""; sub(/^ +/, ""); print count " " $0}') )
 for ((i=0; i<${#results[@]}; i+=2)); do
     echo "$((i/2+1)). ${results[i+1]} - ${results[i]} requests"
 done
